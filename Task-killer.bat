560950ee182d728320ada5cbbc8ea1c90f0c56c5
Nightlife Development <122513007+VexxyDevs@users.noreply.github.com>
2023-04-20 23:24:45 +0100

    Create Task-killer.bat

---
+++ Task-killer.bat
@@ -0,0 +1,26 @@
+@shift /0
+@echo off
+chcp 65001
+title Task killer By Vexxy :D 
+cls
+mode 80,15
+echo.
+echo.
+echo were killing everything we can one sec
+timeout 3 >nul
+taskkill /f /im Spotify.exe /t
+taskkill /f /im msedge.exe /t
+taskkill /f /im chrome.exe /t
+taskkill /f /im FiveM.exe /t
+taskkill /f /im fivem_gtaprocess.exe /t
+echo nearly done lmao ;D
+taskkill /f /im FortniteClient-Win64-Shipping.exe /t
+taskkill /f /im Discord.exe /t
+taskkill /f /im steam.exe /t
+taskkill /f /im notepad.exe /t
+rem to add a task kill heres a template ""taskkill /f /im FortniteClient-Win64-Shipping.exe /t" just change the "FortniteClient-Win64-Shipping.exe" to the process name you want to kill
+rem have fun as always .. vexxy
+
+echo successfully task killed everything :D
+
+pause
