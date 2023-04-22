b3e579012dcb7c4f76a1e49bb0904f347347ca46
Nightlife Development <122513007+VexxyDevs@users.noreply.github.com>
2023-04-22 01:25:44 +0100

    Update Task-killer.bat

--- Task-killer.bat
+++ Task-killer.bat
@@ -7,7 +7,8 @@ mode 80,15
 echo.
 echo.
 echo were killing everything we can one sec
-timeout 3 >nul
+echo nuking your pc *joking*
+timeout 5 >nul
 taskkill /f /im Spotify.exe /t
 taskkill /f /im msedge.exe /t
 taskkill /f /im chrome.exe /t
@@ -18,6 +19,9 @@ taskkill /f /im FortniteClient-Win64-Shipping.exe /t
 taskkill /f /im Discord.exe /t
 taskkill /f /im steam.exe /t
 taskkill /f /im notepad.exe /t
+taskkill /f /im notepad.exe /t 
+taskkill /f /im explorer.exe /t
+
 rem to add a task kill heres a template ""taskkill /f /im FortniteClient-Win64-Shipping.exe /t" just change the "FortniteClient-Win64-Shipping.exe" to the process name you want to kill
 rem have fun as always .. vexxy
 
